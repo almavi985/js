2090c65374bf1bfbdd7b43afbe2bfbee9c7c4de5
alma <alhernandez.c02@ucamp.utel.edu.mx>
2021-09-08 17:28:48 -0500

    funciones de js

---
+++ main.js
@@ -0,0 +1,45 @@
+ // se ejecuta el console log conforme se van encontrando 
+ 
+ console.log("Hola");// imprimir en consola  saber quien mando a imprimir 
+alert("Hola de segundo ")
+
+var nombre;
+console .log(nombre);
+
+var nombre= "Pedro";
+console .log(nombre);
+
+ nombre= "otronombre";
+ console .log(nombre);
+ 
+ const id =888;
+ console.log(id);
+
+ nombre = 'Otro\' \' Nombre';
+ console.log(nombre);//concatenar 
+ 
+
+ function miPrimeraFuncion(nombre) {
+    console.log('Hola, qué tal? ' + nombre);
+}
+
+miPrimeraFuncion('Ramón');
+miPrimeraFuncion(156);
+miPrimeraFuncion(true);
+
+
+var segundaFuncion = function (edad) {
+    console.log('Esta es una expresión de función ' + edad);
+}
+
+segundaFuncion(15);
+
+
+(function (nombre) {
+    console.log('Hola, qué tal? ' + nombre);
+})('Iván')
+
+
+
+
+
